28dbd5e53f7de13312ba76442f88203916635623
Clay Tsay <claytsay@users.noreply.github.com>
2019-01-15 12:40:37 -0800

    Updates

---
+++ precache-manifest.52f847caacb108696fca3de0f5f6535e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/person_simulator/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b8bb5b8ab3e614f8d657",
+    "url": "/person_simulator/static/js/runtime~main.b8bb5b8a.js"
+  },
+  {
+    "revision": "5d8497ac04d10d417260",
+    "url": "/person_simulator/static/js/main.5d8497ac.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/person_simulator/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "5d8497ac04d10d417260",
+    "url": "/person_simulator/static/css/main.60141933.chunk.css"
+  },
+  {
+    "revision": "d06836cf9060e995ad4b4936afb28a5e",
+    "url": "/person_simulator/index.html"
+  }
+];
