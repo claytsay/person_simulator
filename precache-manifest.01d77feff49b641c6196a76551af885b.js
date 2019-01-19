4baeb46741a2ae8f866c98d76f676d1d79e1a774
Clay Tsay <claytsay@users.noreply.github.com>
2019-01-19 09:15:52 -0800

    Updates

---
+++ precache-manifest.01d77feff49b641c6196a76551af885b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/person_simulator/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b8bb5b8ab3e614f8d657",
+    "url": "/person_simulator/static/js/runtime~main.b8bb5b8a.js"
+  },
+  {
+    "revision": "6db860bc962695063fcb",
+    "url": "/person_simulator/static/js/main.6db860bc.chunk.js"
+  },
+  {
+    "revision": "ad961163a52a17acddf4",
+    "url": "/person_simulator/static/js/1.ad961163.chunk.js"
+  },
+  {
+    "revision": "6db860bc962695063fcb",
+    "url": "/person_simulator/static/css/main.2c8257b6.chunk.css"
+  },
+  {
+    "revision": "5fdf2612137be0ef4f6651096293c33b",
+    "url": "/person_simulator/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/person_simulator/precache-manifest.881e61b932d15247f8d31c154561488a.js"
+  "/person_simulator/precache-manifest.01d77feff49b641c6196a76551af885b.js"
 );
 
 workbox.clientsClaim();
